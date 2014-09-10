f813eab38a2481922a7f924a0471b12de19b7a60
Bernhard Mallinger <b.mallinger@gmx.at>
2014-09-10 17:42:47 +0200

    intermediate

--- thesis/spell.sh
+++ thesis/spell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd chapters
-for i in beth.tex huang_proof_original.tex huang_proof_without_P_P.tex interpolation.tex introduction.tex  lk.tex lyndon.tex  maehara.tex preliminaries.tex reduction.tex resolution.tex   semantic_proof.tex subtermrelation-nested.tex ; do
+for i in beth.tex huang_proof_original.tex huang_proof_without_P_P.tex interpolation.tex introduction.tex  lk.tex lyndon.tex  maehara.tex preliminaries.tex reduction.tex resolution.tex   semantic_proof.tex subtermrelation-nested.tex quant_alt.tex; do
 	aspell -l en_US -t check $i
 done
 
