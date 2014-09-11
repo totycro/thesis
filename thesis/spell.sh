72f661dc1134477e058edd860d2e16a34705fb87
totycro <totycro@unknown-horizons.org>
2014-09-11 11:28:49 +0200

    intermediate

--- thesis/spell.sh
+++ thesis/spell.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+aspell -l en_US -t check thesis.tex
+
 cd chapters
-for i in beth.tex huang_proof_original.tex huang_proof_without_P_P.tex interpolation.tex introduction.tex  lk.tex lyndon.tex  maehara.tex preliminaries.tex reduction.tex resolution.tex   semantic_proof.tex subtermrelation-nested.tex quant_alt.tex; do
+for i in beth.tex huang_proof_original.tex huang_proof_without_P_P.tex interpolation.tex introduction.tex  lk.tex lyndon.tex  maehara.tex preliminaries.tex reduction.tex resolution.tex   semantic_proof.tex subtermrelation-nested.tex quant_alt.tex conclusion.tex; do
 	aspell -l en_US -t check $i
 done
 
